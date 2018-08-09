fff250b559369a645d68680cc559ed03ee8418cc
samisnotinsane <sameen11@hotmail.com>
2018-08-09 19:28:12 +0100

    Adding login class.

---
+++ app/src/main/java/com/burn/burn/controller/login/Login.java
@@ -0,0 +1,14 @@
+package com.burn.burn.controller.login;
+
+public class Login {
+
+    private String userId;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
