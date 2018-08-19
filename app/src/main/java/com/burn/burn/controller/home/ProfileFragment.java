9ce6a3561537983d52f123f7f4e8dd6c3e88cf31
samisnotinsane <sameen11@hotmail.com>
2018-08-19 21:01:56 +0100

    Adding form for viewing profile information under profile tab.

--- app/src/main/java/com/burn/burn/controller/home/ProfileFragment.java
+++ app/src/main/java/com/burn/burn/controller/home/ProfileFragment.java
@@ -1,11 +1,14 @@
 package com.burn.burn.controller.home;
 
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.burn.burn.R;
 
@@ -23,8 +26,20 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View fragmentView = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        Spinner spinner = (Spinner) fragmentView.findViewById(R.id.spinner_gender);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity().getBaseContext(),
+                R.array.gender_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+        return fragmentView;
     }
 
 }
